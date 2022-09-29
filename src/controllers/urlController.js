59c34ceac0e086dec9c7cdf09351d928b495db4f
TarunKumar <tarunkumarkoli.tk@gmail.com>
2022-09-29 15:58:01 +0530

    second commit from tarun

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -8,7 +8,6 @@ const validUrl = function (value) {
     if (value == undefined) { return "Url is mandatory" }
     if (typeof value !== "string") { return "Url must be in string" }
     if (value.trim() == "") { return "Url can not be empty" }
-    // return value.toLowerCase().trim()
     if (!urlValidation(value)) { return "Invalid URL" }
     return true
 }
@@ -25,16 +24,17 @@ const shortenURL = async function (req, res) {
 
         if (Object.keys(rest).length > 0) return res.status(400).send({ status: false, message: `You can not fill these:-( ${Object.keys(rest)} ) data ` })
 
-
         if (validUrl(originalUrl) != true) return res.status(400).send({ status: false, message: `${validUrl(originalUrl)}` })
-        if (!urlValidation(originalUrl)) return res.status(400).send({ status: false, message: "please enter valid url" })
 
 
         let url_in_DB = await urlModel.findOne({ longUrl: originalUrl }).select({ _id: 0, updatedAt: 0, createdAt: 0, __v: 0 })
-        if (url_in_DB) return res.status(409).send({ status: false, message: "URL is already present", shortUrl: url_in_DB.urlCode })
+        if (url_in_DB) return res.status(409).send({ status: false, message: "LongUrl is already present", shortUrl: url_in_DB.urlCode })
 
 
         let urlCode = shortid.generate().toLowerCase()
+
+        let shortUrl_in_DB = await urlModel.findOne({ urlCode: urlCode })
+        if (shortUrl_in_DB) return res.status(409).send({ status: false, message: "shortUrl is already present" })
 
         let baseurl = "http://localhost:3000/"
         let shortUrl = baseurl + urlCode
@@ -42,8 +42,7 @@ const shortenURL = async function (req, res) {
 
 
         let data = await urlModel.create({ longUrl, shortUrl, urlCode })
-        // return res.redirect('/facebook.com')
-        return res.status(201).send({ status: false, message: "successfully created", data: data })
+        return res.status(201).send({ status: true, message: "sortUrl successfully created", data: data })
 
     }
     catch (err) {
@@ -62,7 +61,7 @@ const getUrl = async function (req, res) {
         let url = await urlModel.findOne({ urlCode: urlCode }).select({ longUrl: 1, _id: 0 })
         if (!url) return res.status(404).send({ status: false, message: `${urlCode} urlCode not found` })
 
-        return res.status(302).send({ status: true, message: url })
+        return res.status(302).redirect(url.longUrl)
 
 
 
